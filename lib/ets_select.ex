d5f0600570c7504afe5a273541a4bac5e00c30a5
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 00:52:20 +0200

    Build

--- lib/ets_select.ex
+++ lib/ets_select.ex
@@ -1,2 +1,12 @@
 defmodule EtsSelect do
+  def build(%{and: and_conditions}) do
+  end
+
+  def build(%{or: or_conditions}) do
+  end
+
+  def build(simple_conditions) do
+    list = Enum.map(simple_conditions, fn {k, v} -> Map.new([{k, v}]) end)
+    build(%{and: list})
+  end
 end
