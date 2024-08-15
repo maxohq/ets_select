eb4c0157c6926cc0a29402e57039a5131e46ab2b
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 01:15:28 +0200

    Chore: kinda working

--- lib/ets_select.ex
+++ lib/ets_select.ex
@@ -1,12 +1,40 @@
 defmodule EtsSelect do
   def build(%{and: and_conditions}) do
+    match_spec =
+      Enum.reduce(and_conditions, %{}, fn [_, key, value], acc ->
+        Map.put(acc, key, value)
+      end)
+
+    [{
+      {:"$1", match_spec},
+      [],
+      [:"$_"]
+    }]
   end
 
   def build(%{or: or_conditions}) do
+    {match_head, guard, result} = build_or_condition(or_conditions)
+    [{match_head, guard, result}]
   end
 
-  def build(simple_conditions) do
-    list = Enum.map(simple_conditions, fn {k, v} -> Map.new([{k, v}]) end)
-    build(%{and: list})
+  defp build_or_condition(conditions) do
+    {match_spec, guards} =
+      Enum.reduce(conditions, {%{}, []}, fn [op, key, value], {match_spec, guards} ->
+        var = :"$#{map_size(match_spec) + 2}"
+        updated_match_spec = Map.put(match_spec, key, var)
+        updated_guards = [{:orelse, {op, var, value}} | guards]
+        {updated_match_spec, updated_guards}
+      end)
+
+    match_head = {:"$1", match_spec}
+    guard = [List.foldr(guards, false, fn {_, guard}, acc -> {:orelse, guard, acc} end)]
+    result = [:"$_"]
+
+    {match_head, guard, result}
+  end
+
+  def build(simple_conditions) when is_map(simple_conditions) do
+    conditions = Enum.map(simple_conditions, fn {k, v} -> [:=, k, v] end)
+    build(%{and: conditions})
   end
 end
--- lib/ets_select_test.exs
+++ lib/ets_select_test.exs
@@ -1,3 +1,56 @@
 defmodule EtsSelectTest do
   use ExUnit.Case
+
+  test "build with OR conditions" do
+    query = %{or: [[:=, :status, :new], [:=, :status, :old]]}
+    expected = [
+      {
+        {:"$1", %{status: :"$2"}},
+        [{:orelse, {:==, :"$2", :new}, {:==, :"$2", :old}}],
+        [:"$_"]
+      }
+    ]
+    assert EtsSelect.build(query) == expected
+  end
+
+  test "build with complex OR conditions" do
+    query = %{or: [[:=, :age, 30], [:=, :age, 35], [:=, :name, "Bob"]]}
+    expected = [
+      {
+        {:"$1", %{name: :"$2", age: :"$3"}},
+        [{:orelse,
+          {:==, :"$3", 30},
+          {:orelse,
+            {:==, :"$3", 35},
+            {:==, :"$2", "Bob"}
+          }}],
+        [:"$_"]
+      }
+    ]
+    assert EtsSelect.build(query) == expected
+  end
+
+  test "build with AND conditions" do
+    query = %{and: [[:=, :age, 30], [:=, :name, "Bob"]]}
+    expected = [
+      {
+        {:"$1", %{age: 30, name: "Bob"}},
+        [],
+        [:"$_"]
+      }
+    ]
+    assert EtsSelect.build(query) == expected
+  end
+
+  test "build with simple conditions" do
+    query = %{age: 30, name: "Bob"}
+    expected = [
+      {
+        {:"$1", %{age: 30, name: "Bob"}},
+        [],
+        [:"$_"]
+      }
+    ]
+    assert EtsSelect.build(query) == expected
+  end
 end
