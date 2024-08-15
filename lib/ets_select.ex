94c994c637ac461ca8499a121e2830f620e80d62
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 00:36:37 +0200

    Init

---
+++ lib/ets_select.ex
@@ -0,0 +1,18 @@
+defmodule EtsSelect do
+  @moduledoc """
+  Documentation for `EtsSelect`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EtsSelect.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ets_select_test.exs
@@ -0,0 +1,8 @@
+defmodule EtsSelectTest do
+  use ExUnit.Case
+  doctest EtsSelect
+
+  test "greets the world" do
+    assert EtsSelect.hello() == :world
+  end
+end
