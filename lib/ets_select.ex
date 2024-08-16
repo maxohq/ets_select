912b692c89ff06dd0bf254b4b54bd8f449c1a70a
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 02:00:03 +0200

    Feat: working impl

--- lib/ets_select.ex
+++ lib/ets_select.ex
@@ -1,43 +1,41 @@
 defmodule EtsSelect do
-  def build(%{and: and_conditions}) do
-    {match_spec, guards} =
-      Enum.reduce(and_conditions, {%{}, []}, fn [op, key, value], {match_spec, guards} ->
-        var = :"$#{map_size(match_spec) + 2}"
-        updated_match_spec = Map.put(match_spec, key, var)
-        updated_guards = [{op, var, value} | guards]
-        {updated_match_spec, updated_guards}
-      end)
-
-    match_head = {:"$1", match_spec}
-    guard = [List.foldr(guards, true, fn guard, acc -> {:andalso, guard, acc} end)]
-    result = [:"$_"]
-
-    [{match_head, guard, result}]
+  def build(%{and: and_conditions, project: project}) do
+    build_condition(:and, and_conditions, project)
   end
 
-  def build(%{or: or_conditions}) do
-    {match_head, guard, result} = build_or_condition(or_conditions)
-    [{match_head, guard, result}]
+  def build(%{or: or_conditions, project: project}) do
+    build_condition(:or, or_conditions, project)
   end
+
+  def build(%{and: and_conditions}), do: build(%{and: and_conditions, project: nil})
+  def build(%{or: or_conditions}), do: build(%{or: or_conditions, project: nil})
 
   def build(simple_conditions) when is_map(simple_conditions) do
     conditions = Enum.map(simple_conditions, fn {k, v} -> [:=, k, v] end)
     build(%{and: conditions})
   end
 
-  defp build_or_condition(conditions) do
+  defp build_condition(type, conditions, project) do
     {match_spec, guards} =
       Enum.reduce(conditions, {%{}, []}, fn [op, key, value], {match_spec, guards} ->
-        var = :"$#{map_size(match_spec) + 2}"
+        var = Map.get(match_spec, key, :"$#{map_size(match_spec) + 2}")
         updated_match_spec = Map.put(match_spec, key, var)
         updated_guards = [{op, var, value} | guards]
         {updated_match_spec, updated_guards}
       end)
 
     match_head = {:"$1", match_spec}
-    guard = [List.foldr(guards, false, fn guard, acc -> {:orelse, guard, acc} end)]
-    result = [:"$_"]
+    guard = [build_guard(type, Enum.reverse(guards))]
+    result = build_project(match_spec, project)
 
-    {match_head, guard, result}
+    [{match_head, guard, result}]
+  end
+
+  defp build_guard(:and, guards), do: Enum.reduce(guards, &{:andalso, &2, &1})
+  defp build_guard(:or, guards), do: Enum.reduce(guards, &{:orelse, &2, &1})
+
+  defp build_project(match_spec, nil), do: [:"$_"]
+  defp build_project(match_spec, project) do
+    [List.to_tuple(Enum.map(project, fn field -> Map.get(match_spec, field) end))]
   end
 end
