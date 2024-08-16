1ca335cd40f87a29d3451f2fa5c5c3ca9ca2d62f
Roman Heinrich <roman.heinrich@gmail.com>
2024-08-16 03:18:45 +0200

    Chore: prepare release

--- mix.exs
+++ mix.exs
@@ -1,15 +1,21 @@
 defmodule EtsSelect.MixProject do
   use Mix.Project
+  @github_url "https://github.com/maxohq/ets_select"
+  @version "0.1.0"
+  @description "ETS match spec builder from a simple intuitive query language"
 
   def project do
     [
       app: :ets_select,
-      version: "0.1.0",
+      source_url: @github_url,
+      version: @version,
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      package: package(),
       test_paths: ["lib"],
-      test_pattern: "*_test.exs"
+      test_pattern: "*_test.exs",
+      description: @description
     ]
   end
 
@@ -17,6 +23,17 @@ defmodule EtsSelect.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp package do
+    [
+      files: ~w(lib mix.exs README* CHANGELOG* LICENCE*),
+      licenses: ["MIT"],
+      links: %{
+        "Github" => @github_url,
+        "Changelog" => "#{@github_url}/blob/main/CHANGELOG.md"
+      }
     ]
   end
 
